3f36c97bff9b889c68d02ea4f306486b5d112871
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-02-25 23:46:45 +0000

    make more platform independent

--- src/env_validate/env_validate.py
+++ src/env_validate/env_validate.py
@@ -1,33 +1,31 @@
 import os
-import subprocess
-import tempfile
-from os import path
 import logging
 
 log = logging.getLogger(__name__)
 
 
+def remove_comments(lines):
+    return [
+        line for line in lines if line.strip() and not line.startswith("#")
+    ]  # noqa: E501
+
+
 def validate_env():
-    if path.exists(".env.example"):
-        env_example = subprocess.check_output(
-            "awk -F '=' 'NF {print $1}' .env.example | grep --regexp=^[a-zA-Z]",  # noqa
-            shell=True,
-        )
-        fp = tempfile.TemporaryFile()
-        fp.write(env_example)
-        fp.seek(0)
-        required_envs = fp.readlines()
+    env_file = os.path.join(os.getcwd(), ".env.example")
+    if os.path.exists(env_file):
+        with open(env_file, "r") as f:
+            env_example = f.read()
+        required_envs = [
+            env.split("=")[0].strip()
+            for env in remove_comments(env_example.split("\n"))
+        ]
     else:
-        required_envs = open("required_envs").readlines()
+        with open("required_envs", "r") as f:
+            required_envs = remove_comments(f.read().splitlines())
 
     missing = []
     found_missing = False
     for env in required_envs:
-        try:
-            env = env.decode("utf-8").strip()
-        except AttributeError:
-            env = env.strip()
-
         if env not in os.environ:
             found_missing = True
             missing.append(env)
